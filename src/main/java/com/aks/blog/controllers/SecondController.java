49922a0673e40b1772c1935b9a8ea569d8bfec9c
Alexander Strakh <astrakh@PMINTL.NET>
2021-05-21 14:11:30 +0300

    Fix bugs

--- src/main/java/com/aks/blog/controllers/SecondController.java
+++ src/main/java/com/aks/blog/controllers/SecondController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// TODO: Sergey should remove this controller to prevent unused files
 @Controller
 public class SecondController {
 
