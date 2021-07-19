81e886b234cad6c59955a3f5902679ded1a6bac0
Alexander Strakh <astrakh@PMINTL.NET>
2021-07-19 19:59:35 +0300

    Initial commit

---
+++ skrylov-package/src/main/resources/postinstall.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Ensure our service is enabled
+echo "";
+echo "____  _            _  __          ___ __   ";
+echo "|  _ \| |          | | \ \        / (_) |  ";
+echo "| |_) | | ___   ___| | _\ \  /\  / / _| |_ ";
+echo "|  _ <| |/ _ \ / __| |/ /\ \/  \/ / | | __|";
+echo "| |_) | | (_) | (__|   <  \  /\  /  | | |_ ";
+echo "|____/|_|\___/ \___|_|\_\  \/  \/   |_|\__|";
+echo "";
+
+COMMON_NAME="skrylov";
+USER_NAME=$COMMON_NAME;
+GROUP_NAME=$COMMON_NAME;
+SERVICE_NAME=$COMMON_NAME;
+
+SERVICE=$SERVICE_NAME;
+PROJECT_REL_FOLDER_NAME=$COMMON_NAME;
+PROJECT_HOME_DIR="/opt/$PROJECT_REL_FOLDER_NAME";
+PROJECT_HOME_CONFIG_DIR="$PROJECT_HOME_DIR/config";
+PROJECT_HOME_CONFIG="$PROJECT_HOME_CONFIG_DIR/application.properties";
+PROJECT_HOME_CONFIG_EXAMPLE="$PROJECT_HOME_CONFIG_DIR/application.properties.example";
+PROJECT_USERNAME=$USER_NAME;
+
+
+if [ -f "$PROJECT_HOME_CONFIG" ]; then
+  echo "$SERVICE config file - $PROJECT_HOME_CONFIG - already exists. Please, update file corresponding with $PROJECT_HOME_CONFIG_EXAMPLE";
+else
+  echo "$SERVICE config file - $PROJECT_HOME_CONFIG not created before";
+  cp $PROJECT_HOME_CONFIG_EXAMPLE $PROJECT_HOME_CONFIG;
+  chown $PROJECT_USERNAME:$PROJECT_USERNAME $PROJECT_HOME_CONFIG;
+  echo "Copy $PROJECT_HOME_CONFIG_EXAMPLE to $PROJECT_HOME_CONFIG, please configure it and start service";
+fi
+
+if [ ! -d $PROJECT_HOME_CONFIG_DIR ]; then
+  mkdir -p $PROJECT_HOME_CONFIG_DIR;
+  chown $PROJECT_USERNAME:$PROJECT_USERNAME -R $PROJECT_HOME_CONFIG_DIR;
+fi
+
+systemctl daemon-reload;
+
+if [ $1 -eq 1 ]; then
+  echo "Service haven't installed before. Enabling $SERVICE autostart after boot...";
+  systemctl enable $SERVICE >/dev/null 2>&1 || :
+  echo "Service autostart enabled";
+else
+  echo "Service installed before. Restarting $SERVICE ...";
+  systemctl restart $SERVICE >/dev/null 2>&1 || :
+  echo "Restart finished. If you update old version of $SERVICE you should check service status";
+fi
+
+echo "Service $SERVICE successfully installed!"
+if [ $1 -eq 1 ]; then
+  echo "This is first installation of service $SERVICE.";
+  echo "You should configure service properties in this file $PROJECT_HOME_CONFIG.";
+  echo "And then run start service command: sudo systemctl start $SERVICE";
+fi
+
+
+
---
+++ skrylov-package/src/main/resources/preremove.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+COMMON_NAME="skrylov";
+USER_NAME=$COMMON_NAME;
+GROUP_NAME=$COMMON_NAME;
+SERVICE_NAME=$COMMON_NAME;
+
+# Ensure our service is enabled
+PROJECT_REL_FOLDER_NAME=$COMMON_NAME;
+PROJECT_HOME_DIR="/opt/$PROJECT_REL_FOLDER_NAME";
+
+PROJECT_HOME_CONFIG_DIR="$PROJECT_HOME_DIR/config";
+PROJECT_HOME_CONFIG="$PROJECT_HOME_CONFIG_DIR/application.properties";
+PROJECT_HOME_CONFIG_EXAMPLE="$PROJECT_HOME_CONFIG_DIR/application.properties.example";
+PROJECT_USERNAME=$USER_NAME;
+SERVICE=$SERVICE_NAME;
+
+systemctl stop $SERVICE >/dev/null 2>&1 || :
+systemctl disable $SERVICE >/dev/null 2>&1 || :
+
+
