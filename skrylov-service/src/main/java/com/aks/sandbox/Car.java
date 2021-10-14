5cb7b2b5e9cc918ff020fad6c5cd0276417c6fa1
mrserg86 <skrylov@blockwit.io>
2021-10-15 01:05:43 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

--- skrylov-service/src/main/java/com/aks/sandbox/Car.java
+++ skrylov-service/src/main/java/com/aks/sandbox/Car.java
@@ -2,8 +2,5 @@ package com.aks.sandbox;
 
 public class Car {
     private int mass;
-    public Car(int mass) {
-        this.mass = mass;
-        Engine Engine1 = new Engine(86);
-    };
+    private Engine E1;
 }
