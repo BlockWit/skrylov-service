9a46e66a340aad1635ceebc92fdc2cad3dfbdedf
mrserg86 <skrylov@blockwit.io>
2021-10-15 19:33:53 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

---
+++ skrylov-service/src/main/java/com/aks/sandbox/case1/Pill.java
@@ -0,0 +1,37 @@
+package com.aks.sandbox.case1;
+
+public class Pill {
+
+    private long issueDate;
+
+    private long expirationPeriod;
+
+    public Pill(long issueDate, long expirationPeriod) {
+        this.issueDate = issueDate;
+        this.expirationPeriod = expirationPeriod;
+    }
+
+    public void setIssueDate(long issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public long getIssueDate() {
+        return issueDate;
+    }
+
+    public void setExpirationPeriod(long expirationPeriod) {
+        this.expirationPeriod = expirationPeriod;
+    }
+
+    public long getExpirationPeriod() {
+        return expirationPeriod;
+    }
+
+    public boolean isSpoiled() {
+        if ((System.currentTimeMillis() - issueDate) > expirationPeriod) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
