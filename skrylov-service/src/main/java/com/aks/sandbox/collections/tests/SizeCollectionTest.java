7cf1d77010728d0f51a53debccedc15929abae32
mrserg86 <skrylov@blockwit.io>
2021-10-19 00:53:15 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

--- skrylov-service/src/main/java/com/aks/sandbox/collections/tests/SizeCollectionTest.java
+++ skrylov-service/src/main/java/com/aks/sandbox/collections/tests/SizeCollectionTest.java
@@ -12,10 +12,10 @@ public class SizeCollectionTest {
         collection2.add(2);
         collection2.del(1);
 
-        int p = collection2.size;
+        int p = collection2.size();
         System.out.println(p);
 
-        if (collection2.size == 3) {
+        if (collection2.size() == 3) {
             System.out.println("OK");
         } else {
             System.out.println("Fail");
