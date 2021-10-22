13c2274f1d8c3a80ca500796e0fdf3cd047d09a4
mrserg86 <skrylov@blockwit.io>
2021-10-23 01:17:16 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

---
+++ skrylov-service/src/main/java/com/aks/sandbox/Dog.java
@@ -0,0 +1,37 @@
+package com.aks.sandbox;
+
+public class Dog {
+
+    private String name = "Guchka";
+
+    private int volumeStomach;
+
+    public Dog(String name, int volumeStomach) {
+        this.name = name;
+        this.volumeStomach = volumeStomach;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setVolumeStomach(int volumeStomach) {
+        this.volumeStomach = volumeStomach;
+    }
+    public int getVolumeStomach() {
+        return volumeStomach;
+    }
+    public void volumeFood(int volumeNewFood) {
+        int totalVolumeFood = 0;
+        totalVolumeFood = totalVolumeFood + volumeNewFood;
+    }
+    public String satiety(int totalVolumeFood) {
+        String infoSatiety;
+        if (totalVolumeFood > volumeStomach/2) {
+            infoSatiety = "Сыта";
+        } else {
+            infoSatiety = "Голодная";
+        }
+    }
+}
