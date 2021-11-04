23afb1dc3c061322bbafdc5909819770deb94a8e
mrserg86 <skrylov@blockwit.io>
2021-11-04 23:34:48 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

---
+++ skrylov-service/src/main/java/com/aks/sandbox/cases/case5/Nigger.java
@@ -0,0 +1,30 @@
+package com.aks.sandbox.cases.case5;
+
+public class Nigger {
+
+    private int mass;
+
+    private Nigger nigger;
+
+    public Nigger(int mass, Nigger nigger) {
+        this.mass = mass;
+        this.nigger = nigger;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setNigger(Nigger nigger) {
+        this.nigger = nigger;
+    }
+
+    public Nigger getNigger() {
+        return nigger;
+    }
+
+}
---
+++ skrylov-service/src/main/java/com/aks/sandbox/cases/case5/Precipice.java
@@ -0,0 +1,35 @@
+package com.aks.sandbox.cases.case5;
+
+public class Precipice {
+
+    private int breakingPoint;
+
+    private Nigger firstNigger;
+
+    public Precipice(int breakingPoint, Nigger firstNigger) {
+        this.breakingPoint = breakingPoint;
+        this.firstNigger = firstNigger;
+    }
+
+    public void setBreakingPoint(int breakingPoint) {
+        this.breakingPoint = breakingPoint;
+    }
+
+    public int getBreakingPoint() {
+        return breakingPoint;
+    }
+
+    public void setFirstNigger(Nigger firstNigger) {
+        this.firstNigger = firstNigger;
+    }
+
+    public Nigger getFirstNigger() {
+        return firstNigger;
+    }
+
+    public void clamberFirstNigger(Nigger firstNigger) {
+        this.firstNigger = firstNigger;
+        firstNigger.getMass()
+    }
+
+}
