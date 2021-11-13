881fec41d3b317cace71a066f09ce02871fd94a9
mrserg86 <skrylov@blockwit.io>
2021-11-13 16:20:05 +0300

    Merge remote-tracking branch 'origin/DEV' into DEV

--- skrylov-service/src/main/java/com/aks/sandbox/collections/CollectionByBoxes.java
+++ skrylov-service/src/main/java/com/aks/sandbox/collections/CollectionByBoxes.java
@@ -29,12 +29,11 @@ public class CollectionByBoxes {
 
     public void addElement(Integer nextElementOfCollection) {
         if (firstBox == null) {
-
             count++;
-            Box currentBox;
-            currentBox = firstBox.getNextBox();
+            Box currentBox = firstBox.getNextBox();   //перед этим по идее сначала надо вызвать метод setNextBox, но не получается
             Box firstBox = new Box(nextElementOfCollection, 1, null, currentBox);
             firstBox.setElementOfCollection(nextElementOfCollection);
+
         } else {
             Box currentBox = firstBox;
             currentBox = firstBox.getNextBox();
